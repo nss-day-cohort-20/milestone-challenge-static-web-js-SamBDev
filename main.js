d83d14d11639e7caaa64f8e8fd38e60566fba671
Sam <samb283@gmail.com>
2017-06-07 11:14:34 -0500

    fixed some typos and missed comments. still need button event listeners and some way to make the function quit when alert is thrown

--- main.js
+++ main.js
@@ -4,29 +4,31 @@ function tree (treeParameters) {
 	// loop so the console is logged as heigth parameter indicates
 	for (let i = 0; i < treeParameters.height; i++) {
 		
-		// set up whiteSpace variable so we can call .repeat() on it
+		// set up whiteSpace string variable so we can call .repeat() on it
 		let whiteSpace = " ";
 
-		// calculate the amount of white space need for each line.
+		// calculate the amount of white space needed for each line.
 		// As there is no need to explicitly log white space after 
 		// the characters there is no need to multiply by two
 		// only to divide by two later
 		let neededWhiteSpace = (treeParameters.height - (i+1));
-		
-		// console.log(" ".repeat(neededWhiteSpace/2));
 
-		// 
+		// log line to console in required format.
+		// White space need only be explicitly logged before the characters
+		// as white space after the characters is already handled
 		console.log(`${whiteSpace.repeat(neededWhiteSpace)}${treeParameters.char.repeat(i*2+1)}`);
 	}
 }
 
-// take input values and assigns them as necessary. Runs when event listener condition met
+// take input values and assigns them as necessary. 
+// Runs when event listener condition met
 function userInput(){
 
 	// assign the values of the inputs to temporary variables
 	let inputHeight = document.getElementById("treeHeight").value;
 	let inputChar = document.getElementById("treeChar").value;
 
+	// old test console logs
 	// console.log("function is called");
 	// console.log("input Height is ", inputHeight);
 	// console.log("input char is ", inputChar);
@@ -51,6 +53,7 @@ function userInput(){
 // create an empty object for treeParameters to be used later
 const treeParameters = {};
 
+// add an event listener to the Grow you tree button
 document.getElementById("inputButton").addEventListener("click", userInput);
 
 // document.getElementById("treeHeight").addEventListener("")
